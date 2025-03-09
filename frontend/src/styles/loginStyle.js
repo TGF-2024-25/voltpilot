66785d8cf83979528173cc848eeda88d80aa9aae
Naive <congchanghong@gmail.com>
2025-03-09 19:30:20 +0100

    pantalla login

---
+++ frontend/src/styles/loginStyle.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 16,
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 16,
+      textAlign: 'center',
+    }, 
+    input: {
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginBottom: 12,
+      paddingHorizontal: 8,
+    },
+  });
