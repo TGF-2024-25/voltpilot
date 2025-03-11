860db5a24b17ec85b99d9cef9b62f410231637d1
MarieteCampos <mario.cam.sob@gmail.com>
2025-03-11 17:49:38 +0100

    Fetching places from API places, ready to start route planning

--- frontend/src/views/RoutingScreen.js
+++ frontend/src/views/RoutingScreen.js
@@ -2,6 +2,9 @@ import React, { useState, useRef } from "react";
 import { View } from "react-native";
 import { StyleSheet } from "react-native";
 import MapView, {PROVIDER_DEFAULT} from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
+// eslint-disable-next-line import/no-unresolved
+import { GOOGLE_MAPS_API_KEY } from '@env';
 
 export default function VistaRutas() {
 
@@ -17,16 +20,42 @@ export default function VistaRutas() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.searchBar}>
+       <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Selecciona Destinos"
+        onPress={(data, details = null) => {
+          if (details) {
+            const { lat, lng } = details.geometry.location;
+            const newRegion = {
+              latitude: lat,
+              longitude: lng,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            };
 
-        <MapView 
-          style={styles.map}
-          ref={mapRef}
-          region={region}
-          provider={PROVIDER_DEFAULT}
-          >
+            setRegion(newRegion);
 
-        </MapView>
-
+            mapRef.current?.animateToRegion(newRegion, 1500);
+          }
+        }}
+        query={{
+          key: GOOGLE_MAPS_API_KEY,
+          language: "es",
+        }}
+        styles={{
+          textInput: styles.textInput,
+          listView: styles.listView, 
+        }}
+        />
+      </View>
+      <MapView 
+        style={styles.map}
+        ref={mapRef}
+        region={region}
+        provider={PROVIDER_DEFAULT}
+        >  
+      </MapView>
     </View>
   );
 }
@@ -48,5 +77,16 @@ const styles = StyleSheet.create({
     right: 20,
     zIndex: 1,
     padding: 10,
+  },
+  textInput: {
+    backgroundColor: "white",
+    borderRadius: 25,
+    paddingHorizontal: 15,
+    fontSize: 16,            
+  },
+  listView: {
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    marginTop: 10,            
   },
 })
