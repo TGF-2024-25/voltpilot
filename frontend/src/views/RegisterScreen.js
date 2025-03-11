f7a1b2477604877e3caefa90b473d0639eae126c
Naive <congchanghong@gmail.com>
2025-03-11 15:11:32 +0100

    creando usuario mediante firebase authentic

--- frontend/src/views/RegisterScreen.js
+++ frontend/src/views/RegisterScreen.js
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import styles from "../styles/registerStyle";
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../../backend/src/config/firebaseConfig';
 
 export default function RegisterScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
 
-  const handleRegister = () => {
-    // Handle registration logic here
-    console.log('Register:', email, password, confirmPassword);
+  const handleRegister = async () => {
+    if (password !== confirmPassword) {
+      console.error('Passwords do not match');
+      // Show an error message to the user
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      console.log('Registration successful:', userCredential.user);
+      // Navigate to the login screen or perform other actions
+    } catch (error) {
+      console.error('Registration failed:', error.message);
+      // Show an error message to the user
+    }
   };
 
   return (
