74d2a96c33e7c50159ffee204491018c2c0bcf7e
MarieteCampos <mario.cam.sob@gmail.com>
2025-04-01 18:39:32 +0200

    Fully Changed Route + Functional Routes API request. Pending further expansion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import { Platform } from "react-native";
-import { Text, View } from "react-native";
 // eslint-disable-next-line import/no-unresolved
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from "@react-navigation/native";
+import { MenuProvider } from "react-native-popup-menu";
 import BottomTabNavigator from "./components/BottomTabNavigator";
-import styles from "./styles/appStyle";
-import MapView from "react-native-maps";
 import LoginScreen from './views/LoginScreen';
 import RegisterScreen from './views/RegisterScreen';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Main" component={BottomTabNavigator} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MenuProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+          <Stack.Screen name="Main" component={BottomTabNavigator} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </MenuProvider>
   );
 }
--- frontend/src/views/RoutingScreen.js
+++ frontend/src/views/RoutingScreen.js
@@ -1,96 +1,168 @@
+/* eslint-disable import/no-unresolved */
 import React, { useState, useRef } from "react";
-import { View } from "react-native";
+import { View, TextInput, Button, TouchableOpacity } from "react-native";
 import { StyleSheet } from "react-native";
-import MapView, {Marker, PROVIDER_DEFAULT} from "react-native-maps";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
-// eslint-disable-next-line import/no-unresolved
-import { GOOGLE_MAPS_API_KEY } from '@env';
-//import { Button } from "react-native-web";
-import * as Location from "expo-location"
+import MapView, { Marker, Polyline, PROVIDER_DEFAULT } from "react-native-maps";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_API_KEY } from "@env";
+import * as Location from "expo-location";
+import { Entypo } from '@expo/vector-icons';
+import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
+import polyline from "@mapbox/polyline";
+
 
 export default function VistaRutas() {
-
-  const [origen, setOrigen] = useState({                  // Constante para guardar la ubicación del usuario
+  const [origen, setOrigen] = useState({
     latitude: 40.416775,
     longitude: -3.70379,
     latitudeDelta: 0.1,
     longitudeDelta: 0.1,
   });
-  const [destinos, setDestinos] = useState([]);           // Constante para guardar todos los destinos posibles de la Ruta
-  
+  const [destinos, setDestinos] = useState([]);
+  const [modRuta, setModRuta] = useState(false);
+  const [ruta, setRuta] = useState([]);
+
   const mapRef = useRef(null);
-  
-  React.useEffect(() => {                                 // Solicitar permisos de ubicación al usuario al cargar la pantalla
+
+  React.useEffect(() => {
     getLocationPermission();
-  }, [])
+  }, []);
+
   async function getLocationPermission() {
-    let {status} = await Location.requestForegroundPermissionsAsync();
-    if(status !== 'granted') {
-      alert('Permission Denied');
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission Denied");
       return;
     }
     let location = await Location.getCurrentPositionAsync({});
-    const current = {
+    setOrigen({
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
       latitudeDelta: 0.1,
       longitudeDelta: 0.1,
-    }
-
-    setOrigen(current);
+    });
   }
+
+  const addDestino = () => {
+    setDestinos([...destinos, null]);
+  };
+
+  const fetchRoute = async () => {
+    if (destinos.length === 0 || !destinos[0]) return;
+    
+    const url = `https://routes.googleapis.com/directions/v2:computeRoutes`;
+    const body = {
+      origin: {
+        location: {
+          latLng: {
+            latitude: origen.latitude,
+            longitude: origen.longitude,
+          },
+        },
+      },
+      destination: {
+        location: {
+          latLng: {
+            latitude: destinos[0].latitude,
+            longitude: destinos[0].longitude,
+          },
+        },
+      },
+      travelMode: "DRIVE",
+      routingPreference: "TRAFFIC_AWARE",
+      computeAlternativeRoutes: false,
+      routeModifiers: {
+        avoidTolls: false,
+        avoidHighways: false,
+        avoidFerries: false,
+      },
+      languageCode: "es",
+      units: "METRIC",
+    };
+
+    try{
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Goog-Api-Key": GOOGLE_MAPS_API_KEY,
+          "X-Goog-FieldMask": "routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+      if(data.routes && data.routes.length > 0) { 
+        const polylinePoints = polyline.decode(data.routes[0].polyline.encodedPolyline).map(([lat, lng]) => ({ latitude: lat, longitude: lng }));
+        setRuta(polylinePoints);
+        setModRuta(true);
+      }
+    } catch (error) {
+      console.log("ERROR : Error obtenido en la ruta: ", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <View style={styles.searchBar}>
-       <GooglePlacesAutocomplete
-        fetchDetails={true}
-        placeholder="Selecciona Destinos"
-        onPress={(data, details = null) => {
-          console.log("📡 Datos del lugar seleccionado:", data);
-          console.log("📍 Coordenadas del destino:", details?.geometry?.location);
-
-          if (details) {
-            const { lat, lng } = details.geometry.location;
-            const newDest = {
-              latitude: lat,
-              longitude: lng,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            };
-            // Metemos el nuevo destino en el array de destinos
-            setDestinos([...destinos, newDest]); // Idea: ¿Maximo 3 --> Si se supera el máximo, borrar el más antiguo?
-
-            mapRef.current?.animateToRegion(newDest, 1500);
-          }
-        }}
-        query={{
-          key: GOOGLE_MAPS_API_KEY,
-          language: "es",
-        }}
-        styles={{
-          textInput: styles.textInput,
-          listView: styles.listView, 
-        }}
-        />
-      </View>
-      <MapView 
-        style={styles.map}
-        ref={mapRef}
-        region={origen}
-        provider={PROVIDER_DEFAULT}
-        >  
-      <Marker
-          coordinate={origen}
-          pinColor="lightblue"
-        />
-        {destinos.map((destino, id) => (
-          <Marker
-            key={id}
-            coordinate={destino}
+      <View style={styles.searchContainer}>
+        {!modRuta ? (
+          <GooglePlacesAutocomplete
+            fetchDetails={true}
+            placeholder="Selecciona Destinos"
+            onPress={(data, details = null) => {
+              if (details) {
+                const { lat, lng } = details.geometry.location;
+                setDestinos([{ latitude: lat, longitude: lng }]);
+              }
+            }}
+            query={{ key: GOOGLE_MAPS_API_KEY, language: "es" }}
+            styles={{ textInput: styles.textInput }}
           />
-        ))}
+        ) : (
+          <View>
+            <TextInput style={styles.textInput} value="Ubicación actual" editable={false} />
+            {destinos.map((_, index) => (
+              <GooglePlacesAutocomplete
+                key={index}
+                fetchDetails={true}
+                placeholder="Selecciona destino"
+                onPress={(data, details = null) => {
+                  if (details) {
+                    const { lat, lng } = details.geometry.location;
+                    const updatedDestinos = [...destinos];
+                    updatedDestinos[index] = { latitude: lat, longitude: lng };
+                    setDestinos(updatedDestinos);
+                  }
+                }}
+                query={{ key: GOOGLE_MAPS_API_KEY, language: "es" }}
+                styles={{ textInput: styles.textInput }}
+              />
+            ))}
+          </View>
+        )}
+
+        <Menu>
+          <MenuTrigger>
+            <Entypo name="dots-three-vertical" size={24} color="black" style={styles.menuIcon} />
+          </MenuTrigger>
+          <MenuOptions>
+            <MenuOption onSelect={addDestino} text="Agregar nuevo destino" />
+            <MenuOption onSelect={() => alert("Opciones de ruta")} text="Tipo de Ruta" />
+            <MenuOption onSelect={() => alert("Buscar en ruta")} text="Buscar En Ruta" />
+          </MenuOptions>
+        </Menu>
+      </View>
+
+      <MapView style={styles.map} ref={mapRef} region={origen} provider={PROVIDER_DEFAULT}>
+        <Marker coordinate={origen} pinColor="lightblue" />
+        {destinos.map((destino, id) => destino && <Marker key={id} coordinate={destino} />)}
+      
+        {ruta.length > 0 && <Polyline coordinates={ruta} strokeWidth={5} strokeColor="#ba66ff" />}
       </MapView>
+
+      <TouchableOpacity style={styles.floatingButton} onPress={fetchRoute}>
+        <Button title="Cómo llegar" color="white" onPress={fetchRoute} />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -99,29 +171,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "flex-start",
   },
   map: {
     width: "100%",
     height: "100%",
   },
-  searchBar: {
+  searchContainer: {
     position: "absolute",
     top: 40,
     left: 20,
     right: 20,
     zIndex: 1,
-    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   textInput: {
     backgroundColor: "white",
     borderRadius: 25,
     paddingHorizontal: 15,
-    fontSize: 16,            
+    fontSize: 16,
+    flex: 1,
   },
-  listView: {
-    backgroundColor: "#FFF",
-    borderRadius: 10,
-    marginTop: 10,            
+  floatingButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#007AFF",
+    padding: 15,
+    borderRadius: 30,
+    elevation: 5,
   },
-})
+  menuIcon: {
+    marginLeft: 10,
+  },
+});
